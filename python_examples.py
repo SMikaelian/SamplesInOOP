dd01dd8df1efecb040ed0a85dfbb3fa9c88d18f8
SMikaelian <sarkis.mikaelian.712@my.csun.edu>
2020-05-15 01:53:03 -0700

    added a new Aircraft class and created its instance methods

--- python_examples.py
+++ python_examples.py
@@ -1,10 +1,47 @@
 class Aircraft:
-  kind= 'flying'
+    #class attribute
+    kind= 'flying aircraft'
 
-  def __init__(self, name):
-    self.name= name
+    #initializer/instnace attribute
+    def __init__(self, name, model, category, travelRate, manufacturingDate, status):
+        self.name= name
+        self.model= model
+        self.category= category
+        self.travelRate= travelRate
+        self.manufacturingDate= manufacturingDate
+        self.status= status
+
+    #instance methods
+    def props(self):
+        return "{} is a {} {} and is {}.".format(a.name, a.category, a.kind, a.status)
+
+    def get_name(self):
+        return self._name
+
+    def get_model(self):
+        return self._model
+
+    def get_category(self):
+        return self._category
+
+    def get_travelRate(self):
+        return self._travelRate
+
+    def get_manufacturingDate(self):
+        return self._manufacturingDate
+
+    def get_status(self):
+        return self._status
 
 
-a= Aircraft('A320')
-b= Aircraft('A550')
-c= Aircraft('L720')
+#instantiate Aircraft object
+a= Aircraft('Lockheed Martin F-35 Lightning II', 'F-35', 'air superiority, strike', 'supersonic', 'AUG 2016', 'In-service')
+b= Aircraft('General Dynamics F-16 Fighting Falcon', 'F-16', 'air superiority', 'supersonic', 'JUN 2011', 'In-service')
+c= Aircraft('Lockheed Martin F-22 Raptor', 'F-22', 'air superiority, ground attack, electronic warfare', 'supersonic', 'SEP 2012', 'In-service')
+
+#access the instance attributes
+print("{} is a {} {} and is {}.".format(a.name, a.category, a.kind, a.status))
+
+#calling instance methods
+print(a.props())
+print(b.get_travelRate())
